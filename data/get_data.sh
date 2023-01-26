464dc0560bf57ff21107f379245a7a04deff6409
Richard Luo <luo.richard@gmail.com>
2023-01-25 19:14:49 -0500

    Updated get_data.sh to use both cURL and wget

--- data/get_data.sh
+++ data/get_data.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
-wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
-wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
-wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+if which wget >/dev/null ; then
+    echo "Downloading via wget..."
+    wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+    wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+    wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+    wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+if which curl >/dev/null ; then
+    echo "Downloading via curl..."
+    curl -O -J http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+    curl -O -J http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+    curl -O -J http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+    curl -O -J http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+else
+    echo "Cannot download, neither wget nor curl is available."
+fi
 
 gunzip train-images-idx3-ubyte.gz
 gunzip train-labels-idx1-ubyte.gz
